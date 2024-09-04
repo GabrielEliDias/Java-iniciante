c27a79a295a6928fb831c24907dcdff4d2acac0e
Gabriel <159343630+GabrielEliDias@users.noreply.github.com>
2024-09-04 12:19:45 -0300

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -1,9 +1,17 @@
+
 public class Autor {
     public String nome;
     public String email;
     public String cpf;
 
-    public void info_autor()
+    public Autor(String nome, String email, String cpf){
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+
+    public void mostrar_info_autor()
     {
         System.out.println("--------------------");
         System.out.println("nome: " + this.nome);
