3e2eda2c5010db1d6874576dc50f98932186b20e
Gabriel <159343630+GabrielEliDias@users.noreply.github.com>
2024-09-04 12:19:23 -0300

    Update livro.java

--- livro.java
+++ livro.java
@@ -5,6 +5,15 @@ public class livro {
     public String isbn;
     Autor autor;
 
+    public livro(String nome, String descrição, double valor, String isbn){
+        this.nome = nome;
+        this.descrição = descrição;
+        this.valor = valor;
+        this.isbn = isbn;
+        this.autor = new Autor("robert martin","robertmartin@professor.cesupa.br", "123.456.789.10" );
+
+    }
+
     public void mostrar_detalhes(){
         System.out.println("#########################");
         System.out.println("nome: " + this.nome);
